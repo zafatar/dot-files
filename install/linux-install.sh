5788b99917168ff9fd6bbe1f5b55221f887274a2
zafatar <zaferkiziltoprak@gmail.com>
2020-11-06 16:42:48 +0100

    add new install calls for linux

--- install/linux-install.sh
+++ install/linux-install.sh
@@ -7,3 +7,17 @@ if test ! $(which zsh); then
     echo -e "INFO: Installing `zsh`\n"
     sudo apt-get install zsh
 fi
+
+# Misc. Installations.
+sudo apt-get -y install emacs
+sudo apt-get -y install htop
+sudo apt-get -y install git
+
+# Docker
+sudo apt-get -y install docker.io
+sudo apt-get -y install docker-compose
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# what else...
